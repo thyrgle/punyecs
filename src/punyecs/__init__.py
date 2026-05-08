91ebd5a7883f91698f2a7a4060998aa2c9340a18
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-05-08 13:21:20 -0700

    Passes basic test now!

--- src/punyecs/__init__.py
+++ src/punyecs/__init__.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, Callable
 
 
@@ -6,31 +6,33 @@ from typing import Any, Callable
 class Query:
     """A class that represnets which attributes and objects should be allowed
     (or disallowed) in a group."""
-    and_attr: set[str] = set()
-    exclude_attr: set[str] = set()
-    exclude_obj: set[Any] = set()
+    and_attr: set[str] = field(default_factory=set)
+    exclude_attr: set[str] = field(default_factory=set)
+    exclude_obj: set[Any] = field(default_factory=set)
 
 
 @dataclass
 class World:
-    groups: list[(Query, set, list[Callable[[Any, float]]])] = []
+    groups: list[(Query, list, list[Callable[[Any, float], None]])] = \
+            field(default_factory=list)
 
-    def entity_satisfies_query(entity, query) -> bool:
+    def entity_satisfies_query(self, entity, query) -> bool:
         """Check if an entity should (or should not) be added to a particular
         group by analyzing the query structure."""
-        if entity in query.exclude_obj:
-            return False
+        for e in query.exclude_obj:
+            if entity == e:
+                return False
         for attr in query.and_attr:
-            if not hasattr(entity):
+            if not hasattr(entity, attr):
                 return False
         for attr in query.exclude_attr:
-            if hasattr(entity):
+            if hasattr(entity, attr):
                 return False
         return True
 
     def push_group(self, query: Query):
         """Add the group and return that group."""
-        new_group = (query, set(), [])
+        new_group = (query, [], [])
         self.groups.append(new_group)
         return new_group
 
@@ -39,7 +41,7 @@ class World:
         the entity should belong to."""
         for query, group, funcs in self.groups:
             if self.entity_satisfies_query(entity, query):
-                group.add(entity)
+                group.append(entity)
 
     def update(self, dt: float):
         """Update the world (and all the corresponding groups/entities)."""
@@ -50,7 +52,8 @@ class World:
 
 
 def requirements(world, require: list[str],
-                 exclude: list[str]=None, exclude_obj: list[Any]=None):
+                 exclude: list[str]=None, exclude_obj: list[Any]=None,
+                 *args, **kwargs):
     """Use as a decorator, runs the decorated function on each entity that
     has the required components and none of the excluded components (or
     excluded objects).
@@ -59,16 +62,20 @@ def requirements(world, require: list[str],
     :param exclude: Entity must *not* have the following attributes.
     :param exclude_obj: Exculde individual objects from being ran.
     """
-    exclude = exclude or []
-    exclude_obj = exclude_obj or []
-    query = Query(require, exclude, exclude_obj)
-    group = world.add_group(query)
-    def inner(func):
+    def req_dec(func):
+        nonlocal exclude
+        nonlocal exclude_obj
+        exclude = exclude or []
+        exclude_obj = exclude_obj or []
+        query = Query(require, exclude, exclude_obj)
+        group = world.push_group(query)
         group[2].append(func)
-        return func()
-    return inner
+        def inner(func):
+            return func()
+        return inner
+    return req_dec
 
-def query(world, query: Query):
+def query(world: World, query: Query, *args, **kwarg):
     """Use as a decorator, runs the decorated function on each entity that
     satisfy the query object (similar to ``requirements`` but takes in a 
     Query object directly. ``requirements`` builds a query object.
@@ -76,8 +83,11 @@ def query(world, query: Query):
     :param world: World to query over.
     :param query: Query to execute against.
     """
-    group = world.add_group(query)
-    def inner(func):
-        group[2].append(func)
-        return func()
-    return inner
+    def query_dec(func):
+        group = world.push_group(query)
+        def inner(e, dt):
+            return func(e, dt)
+        group[2].append(inner)
+        print(world.groups)
+        return inner
+    return query_dec
---
+++ tests/test_basic.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from punyecs import World, Query, query
+
+
+def test_query():
+    w = World()
+    q = Query(and_attr={"x", "y"})
+
+    @dataclass
+    class Player:
+        x: float
+        y: float
+
+    @dataclass
+    class Enemy:
+        x: float
+        y: float
+
+    @query(w, q)
+    def f(e, dt):
+        e.x += 0.1
+        e.y += 0.1
+
+    player = Player(0.0, 0.0)
+    enemy = Enemy(0.0, 0.0)
+    w.add(player)
+    w.add(enemy)
+
+    assert player.x == 0
+    assert player.y == 0
+    assert enemy.x == 0
+    assert enemy.y == 0
+
+
+    w.update(1)
+    assert player.x == 0.1
+    assert player.y == 0.1
+    assert enemy.x == 0.1
+    assert enemy.y == 0.1
+
+    w.update(1)
+    assert player.x == 0.2
+    assert player.y == 0.2
+    assert enemy.x == 0.2
+    assert enemy.y == 0.2
